03e4e9787dc467ed83cea81e6bf7bf7679e6b0d0
Rafael <rapha.pse@outlook.com>
2017-06-28 17:10:50 -0300

    Aplicação ERP-CICLE nova

--- ERP-CICLE/src/application/MainController.java
+++ ERP-CICLE/src/application/MainController.java
@@ -1,5 +1,24 @@
 package application;
 
+import java.awt.Label;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MainController {
+	
+	@FXML
+	private Label l;
+	
+	
+	public void acessar(ActionEvent e){
+		
+		l.setText("aaaaaaaaaaaaaaaa");
+		
+		l.getText();
+		
+		
+				
+	}
 	
 }
