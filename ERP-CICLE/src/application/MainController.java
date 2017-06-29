7f83146e06fa511e794337fb4c2a4786f6ed3638
Rafael <rapha.pse@outlook.com>
2017-06-29 19:16:41 -0300

    Adicionando Projeto

---
+++ ERP-CICLE/src/application/MainController.java
@@ -0,0 +1,31 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class MainController {
+	@FXML
+	private ImageView calc;
+	@FXML
+	private AnchorPane p;
+	
+	public MainController() {
+		
+	}
+	public void fechar(){
+		
+	}
+
+	public void t(){
+		
+
+		
+		
+		
+	}
+	
+	
+	
+
+}
